7f413e3c922eb7123dc01b1ef5769d70a9f416dc
Sergey Zholudev <serg.zholudev@gmail.com>
2024-07-18 00:22:11 +0300

    The base version of the WEB_EVENT project. Models are configured and added to the DB. The base code is connected to PostgreSQL.

---
+++ WEB_EVENT/WEB_EVENT/classes.py
@@ -0,0 +1,31 @@
+# classes of the WEB_EVENT project
+
+class User:
+    def __init__(self, name, surname, email, login, age, role, password):
+        # ToDo self.id = generator of id
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.login = login
+        self.age = age
+        self.role = role
+        self.password = password
+
+    def delete(self):
+        pass
+
+    def change_password(self):
+        pass
+
+    def change_email(self):
+        pass
+
+
+class Organizer(User):
+    def __init__(self, name, surname, email, login, age, role, password, phone_number, events_list):
+        super().__init__(name, surname, email, login, age, role, password)
+        self.phone_number = phone_number
+        self.events_list = events_list
+
+    def change_phone(self, new_phone_number) -> None:
+        pass
---
+++ WEB_EVENT/WEB_EVENT/models.py
@@ -0,0 +1,25 @@
+# models of the WEB_EVENT project
+
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    login = models.CharField(max_length=30)
+    age = models.IntegerField()
+    role = models.CharField()
+    password = models.CharField(max_length=30)
+
+
+class Organizer(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    login = models.CharField(max_length=30)
+    age = models.IntegerField()
+    role = models.CharField()
+    password = models.CharField(max_length=30)
+    phone_number = models.IntegerField()
+    # ToDo vents_list = ?
