6ea00b18d78ea385796eea6b64532b15504b3857
Tahn4U <db19012002@mail.ru>
2024-08-07 10:15:18 +0300

    add database connect

--- WEB_EVENT/settings.py
+++ WEB_EVENT/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,23 +87,11 @@ WSGI_APPLICATION = 'WEB_EVENT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'kXbnFqDZxRoGgOSYGqltMDuEEsfGyJjH',
-#         'HOST': 'monorail.proxy.rlwy.net',
-#         'PORT': '12888',
-#     }
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:RQovsJXXcjasTlZlIoYAFWrrxpLWEXNG@monorail.proxy.rlwy.net:14749/railway'
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
