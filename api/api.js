f8780ee9e00eee502f5f5b9c4190810119375b6b
Tahn4U <db19012002@mail.ru>
2024-08-27 15:34:30 +0300

    upd: scr/api.js

--- api/api.js
+++ api/api.js
@@ -1,14 +1,33 @@
-export async function registerUser(userData) {
-    const response = await fetch('https://yourbackend.com/api/signup/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(userData)
+export async function getData() {
+
+    fetch("./header/header.html")
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("header-placeholder").innerHTML = html;
+    })
+    .then(() => {
+      const regButton = document.querySelector("#reg_btn");
+      const reg_form = document.querySelector("#signup_form");
+
+      reg_form.addEventListener("submit", function (e) {
+        e.preventDefault();
+      });
+
+      const formData = {
+        email: document.getElementById("email").value,
+        username: document.getElementById("newUsername").value,
+        password: document.getElementById("newPassword").value,
+        secret_phrase: document.getElementById("secretPhrase").value,
+      };
+
     });
 
+
     if (!response.ok) {
+        alert("Регистрация не удалась, что-то пошло не так(")
         throw new Error('Registration failed');
+    }else{
+        alert("Регистрация успешна!!!")
     }
 
     return await response.json();
--- script.js
+++ script.js
@@ -4,20 +4,4 @@ import { registerUser } from "./api/api.js";
 document.addEventListener("DOMContentLoaded", (e) => {
   initPopup();
 
-  const regButton = document.querySelector("#reg_btn")
-  const reg_form = document.querySelector("#signup_form")
-
-  reg_form.addEventListener("submit", function(e) {
-    e.preventDefault();
-  })
-
-  const formData = {
-    email: document.getElementById("email").value,
-    username: document.getElementById("newUsername").value,
-    password: document.getElementById("newPassword").value,
-    secret_phrase: document.getElementById("secretPhrase").value
-  };
-
-  regButton.addEventListener("click", registerUser(formData))
-
 });
